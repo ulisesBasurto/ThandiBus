a90fec1ce20636c8e0a8c7e0316881d7a1387086
1adangb <adan.no.adan@gmail.com>
2019-10-03 17:49:23 -0500

    Add Android project

---
+++ app/src/main/java/com/metroreal/thandibus/MainActivity.java
@@ -0,0 +1,15 @@
+package com.metroreal.thandibus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //test git
+    }
+}
